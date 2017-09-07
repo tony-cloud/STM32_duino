24be8a4726d2625645194cdd529891228b1aeb6d
huaweiwx <huaweiwx@sina.com>
2017-09-07 12:31:35 +0800

    fix  24c04 to 24c02

--- STM32/variants/REDBULL_V2_F103ZE/i2cEepromConfig.h
+++ STM32/variants/REDBULL_V2_F103ZE/i2cEepromConfig.h
@@ -3,7 +3,7 @@
 #define __BSP_I2CEEPROM_H__
 //#include <Arduino.h>
 
-//#define AT24CXX_TYPE AT24C02  /*standard board use 24c04*/
+//#define AT24CXX_TYPE AT24C02  /*standard board use 24c02*/
 #define AT24CXX_TYPE AT24C512   /*my diy board use 24c512*/
 #define AT24CXX_IIC_DEV	I2C1 	    /*unused i2cport ,SOFT IIC*/
 #define AT24CXX_SCL PB6
