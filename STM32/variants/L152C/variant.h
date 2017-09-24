3c4ca4e46a2a50a3e19d2feb9e2ab58c9149813a
huaweiwx <huaweiwx@sina.com>
2017-09-24 23:33:02 +0800

    add board: STM32L152C

---
+++ STM32/variants/L152C/variant.h
@@ -0,0 +1,31 @@
+#ifndef VARIANT_H
+#define VARIANT_H
+
+/*LED*/
+#ifndef LED_BUILTIN
+#	define LED_BUILTIN PB9
+#endif
+
+#ifndef LED_BUILTIN_MASK
+#	define LED_BUILTIN_MASK		0x00
+#endif
+#define STM32_LED_BUILTIN_ACTIVE_LOW
+
+/*KEY*/
+#define BOARD_NR_KEY	1
+#define BOTTON			PC13  // 2
+#define BOTTON_MASK		0x01
+#define KEYS_LIST  		BOTTON
+
+/*SPI*/
+#define MOSI PA7
+#define MISO PA6
+#define SCK PA5
+#define SS PA4
+
+/*I2C*/
+#define SDA PB7
+#define SCL PB6
+
+/*USB*/
+#endif
