9725465be3f6e6a1bcab02187d19fc619d5300a3
huaweiwx <huaweiwx@sina.com>
2017-10-10 21:47:17 +0800

    update spiflashconfig files

--- STM32/variants/ATKMINI_F103R/i2cEepromConfig.h
+++ STM32/variants/ATKMINI_F103R/i2cEepromConfig.h
@@ -1,6 +1,6 @@
-/* bsp_i2ceeprom.h  iiceeprom config define file ,arduino have not autoload! */
-#ifndef __BSP_I2CEEPROM_H__
-#define __BSP_I2CEEPROM_H__
+/* i2ceepromconfig.h  iic_eeprom config define file ,arduino have not autoload! */
+#ifndef __I2CEEPROMCONFIG_H__
+#define __I2CEEPROMCONFIG_H__
 #include <Arduino.h>
 
 #define AT24CXX_TYPE AT24C02
