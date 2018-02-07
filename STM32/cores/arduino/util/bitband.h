06f247ab326d7864a1ef5979f147641410fe0eab
huaweiwx <huaweiwx@sina.com>
2018-02-08 00:55:24 +0800

    add LL_PIN class

--- STM32/cores/arduino/util/bitband.h
+++ STM32/cores/arduino/util/bitband.h
@@ -5,7 +5,8 @@
   for all stm32 chips 2017.5.28
   add BB functions from maple 2017.10.12 
   add arduino style class BB_PIN 2017.12.20
-
+  for stm32 m0/7 LL_PIN class 
+  
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -42,69 +43,94 @@
                             (((mask)==bit(14))?14: \
                             (((mask)==bit(15))?15:16))))))))))))))))
 
-#if !(defined(STM32F0)||defined(STM32L0)||defined(STM32F7))
+#if (defined(STM32F0)||defined(STM32L0)||defined(STM32F7))
+#define BITBAND_OPTION 0
+#else
+#define BITBAND_OPTION 1 
+#endif
+
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+
+#ifdef GPIOA
+#define PORTA      GPIOA->ODR    //输出 
+#define PINA       GPIOA->IDR    //输入 
+#endif
+#ifdef GPIOB
+#define PORTB      GPIOB->ODR    //输出 
+#define PINB       GPIOB->IDR    //输入 
+#endif
+#ifdef GPIOC
+#define PORTC      GPIOC->ODR                  //输出 
+#define PINC       GPIOC->IDR                  //输入 
+#endif
+#ifdef GPIOD
+#define PORTD      GPIOD->ODR    //输出 
+#define PIND       GPIOD->IDR    //输入 
+#endif
+#ifdef GPIOE
+#define PORTE      GPIOE->ODR    //输出 
+#define PINE       GPIOE->IDR    //输入 
+#endif
+#ifdef GPIOF
+#define PORTF      GPIOF->ODR    //输出 
+#define PINF       GPIOF->IDR    //输入 
+#endif
+#ifdef GPIOG
+#define PORTG      GPIOG->ODR    //输出 
+#define PING       GPIOG->IDR    //输入 
+#endif
+#ifdef GPIOH
+#define PORTH      GPIOH->ODR    //输出 
+#define PINH       GPIOH->IDR    //输入 
+#endif
+#ifdef GPIOI
+#define PORTI      GPIOI->ODR    //输出 
+#define PINI       GPIOI->IDR    //输入 
+#endif
+
+#if BITBAND_OPTION
 //bitband io opration,实现51类似的GPIO控制功能
 #define BITBAND(addr, bitnum) (PERIPH_BB_BASE+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
-#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
 #define BITBAND_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
 
 //n must be less 16!   确保n的值小于16!
 //example:  in = PAin(0);  
 //          PCout(13) = 1;
-
 #ifdef GPIOA
 #define PAout(n)   BITBAND_ADDR((uint32_t)&GPIOA->ODR,n)  //输出 
 #define PAin(n)    BITBAND_ADDR((uint32_t)&GPIOA->IDR,n)  //输入 
-#define PORTA      GPIOA->ODR    //输出 
-#define PINA       GPIOA->IDR    //输入 
 #endif
 #ifdef GPIOB
 #define PBout(n)   BITBAND_ADDR((uint32_t)&GPIOB->ODR,n)  //输出 
 #define PBin(n)    BITBAND_ADDR((uint32_t)&GPIOB->IDR,n)  //输入 
-#define PORTB      GPIOB->ODR    //输出 
-#define PINB       GPIOB->IDR    //输入 
 #endif
 #ifdef GPIOC
 #define PCout(n)   BITBAND_ADDR((uint32_t)&GPIOC->ODR,n)  //输出 
 #define PCin(n)    BITBAND_ADDR((uint32_t)&GPIOC->IDR,n)  //输入 
-#define PORTC      GPIOC->ODR                  //输出 
-#define PINC       GPIOC->IDR                  //输入 
 #endif
 #ifdef GPIOD
 #define PDout(n)   BITBAND_ADDR((uint32_t)&GPIOD->ODR,n)  //输出 
 #define PDin(n)    BITBAND_ADDR((uint32_t)&GPIOD->IDR,n)  //输入 
-#define PORTD      GPIOD->ODR    //输出 
-#define PIND       GPIOD->IDR    //输入 
 #endif
 #ifdef GPIOE
 #define PEout(n)   BITBAND_ADDR((uint32_t)&GPIOE->ODR,n)  //输出 
 #define PEin(n)    BITBAND_ADDR((uint32_t)&GPIOE->IDR,n)  //输入
-#define PORTE      GPIOE->ODR    //输出 
-#define PINE       GPIOE->IDR    //输入 
 #endif
 #ifdef GPIOF
 #define PFout(n)   BITBAND_ADDR((uint32_t)&GPIOF->ODR,n)  //输出 
 #define PFin(n)    BITBAND_ADDR((uint32_t)&GPIOF->IDR,n)  //输入
-#define PORTF      GPIOF->ODR    //输出 
-#define PINF       GPIOF->IDR    //输入 
 #endif
 #ifdef GPIOG
 #define PGout(n)   BITBAND_ADDR((uint32_t)&GPIOG->ODR,n)  //输出 
 #define PGin(n)    BITBAND_ADDR((uint32_t)&GPIOG->IDR,n)  //输入
-#define PORTG      GPIOG->ODR    //输出 
-#define PING       GPIOG->IDR    //输入 
 #endif
 #ifdef GPIOH
 #define PHout(n)   BITBAND_ADDR((uint32_t)&GPIOH->ODR,n)  //输出 
 #define PHin(n)    BITBAND_ADDR((uint32_t)&GPIOH->IDR,n)  //输入
-#define PORTH      GPIOH->ODR    //输出 
-#define PINH       GPIOH->IDR    //输入 
 #endif
 #ifdef GPIOI
 #define PIout(n)   BITBAND_ADDR((uint32_t)&GPIOI->ODR,n)  //输出 
 #define PIin(n)    BITBAND_ADDR((uint32_t)&GPIOI->IDR,n)  //输入
-#define PORTI      GPIOI->ODR    //输出 
-#define PINI       GPIOI->IDR    //输入 
 #endif
 
 //for arduino pin add 2017.10
@@ -119,11 +145,9 @@
 
 #define BB_sramVarPtr(x)  __BB_addr((volatile uint32_t*)&(x), 0, SRAM_BB_BASE, SRAM_BASE)
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 static inline volatile uint32_t* __BB_addr(volatile void*,
                                          uint32_t,
                                          uint32_t,
@@ -204,7 +228,7 @@ static inline volatile uint32_t* __BB_addr(volatile void *address,
 static inline uint32_t BB_pinAdr(uint32_t pin,uint8_t mode){
 	uint32_t portaddr =((mode)?((uint32_t) &variant_pin_list[pin].port->ODR)
 	                          :((uint32_t) &variant_pin_list[pin].port->IDR));
-	uint32_t bit = BITMASKPOS(variant_pin_list[pin].pinMask);
+	uint32_t bit = __builtin_ffs(variant_pin_list[pin].pinMask)-1;
 	return BITBAND(portaddr,bit);
 }
 
@@ -219,12 +243,77 @@ static inline uint32_t BB_pinOutAdr(uint32_t pin){
 	uint32_t bit = __builtin_ffs(variant_pin_list[pin].pinMask)-1;
 	return BITBAND(portaddr,bit);
 }
-
 #ifdef __cplusplus
 } // extern "C"
+#endif
+
+#endif //BITBAND_OPTION
+
+#ifdef __cplusplus
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
+template<const int PinNumber>
+class LL_PIN{
+ public:
+  //----------------------------------------------------------------------------
+  constexpr const uint8_t  pos(void){return variant_gpiopin_pos_static[PinNumber];};
+  constexpr const uint32_t base(void){return variant_gpiopin_base_static[PinNumber];};
+
+  inline LL_PIN & operator = (bool value) __attribute__((always_inline)) {
+    this->write(value);
+    return *this;
+  }
+  
+  inline LL_PIN & operator << (bool value) __attribute__((always_inline)) {
+    this->write(value);
+    return *this;
+  }
+  
+  template<class T> 
+  inline LL_PIN & operator >> (T &value){
+    value = this->read();
+    return *this;
+  }
+
+  inline __attribute__((always_inline))
+  void write(bool value){
+    if (value) {
+        LL_GPIO_SetOutputPin((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));
+    } else {
+        LL_GPIO_ResetOutputPin((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));
+    }
+  }
+
+  inline __attribute__((always_inline))
+  void high(){LL_GPIO_SetOutputPin((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));}
+
+  inline __attribute__((always_inline))
+  void low(){LL_GPIO_ResetOutputPin((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));}
+
+  inline operator bool () const __attribute__((always_inline)) {
+    return this->read();
+  }
+
+  inline __attribute__((always_inline))
+  bool read() const{return LL_GPIO_IsInputPinSet((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));}
+
+/*----- comptabled with DigitalPin ----------*/
+  inline __attribute__((always_inline))
+  void toggle(){LL_GPIO_TogglePin((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber));}
+
+  inline __attribute__((always_inline))
+  void config(uint8_t mode, bool level) {
+      this->write(level);
+	  this->mode(mode);
+  }
+
+  inline __attribute__((always_inline))
+  void mode(uint8_t mode){
+	  pinModeLL((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber), mode);}
+};
+
+#if BITBAND_OPTION
 template<const int PinNumber>
 class BB_PIN{
  public:
@@ -242,7 +331,18 @@ class BB_PIN{
   constexpr const uint32_t bb_outadr(void){return BITBAND(this->portReg(),this->pos());};
 
   inline BB_PIN & operator = (bool value) __attribute__((always_inline)) {
-    MEM_ADDR(this->bb_outadr()) = value;
+    this->write(value);
+    return *this;
+  }
+  
+  inline BB_PIN & operator << (bool value) __attribute__((always_inline)) {
+    this->write(value);
+    return *this;
+  }
+
+  template<class T> 
+  inline BB_PIN & operator >> (T &value){
+    value = this->read();
     return *this;
   }
 
@@ -267,22 +367,20 @@ class BB_PIN{
   void toggle(){write(!MEM_ADDR(this->bb_outadr()));}
 
   inline __attribute__((always_inline))
-  void config(uint8_t mode, bool level) {
+  void config(uint8_t mode, bool value) {
 	  this->mode(mode);
-      MEM_ADDR(this->bb_outadr()) = level;
+      this->write(value);
   }
 
   inline __attribute__((always_inline))
   void mode(uint8_t mode){
 	  pinModeLL((GPIO_TypeDef *)(this->base()), pinTollBitMask(PinNumber), mode);}
 };
+
+#else
+#  define BB_PIN LL_PIN  /*m0/7 use LL_PIN class*/
+#endif //BITBAND_OPTION
 #pragma GCC diagnostic pop
 
 #endif //__cplusplus
-
-#else  // L0/F0/F7
- #warning "BitBand unsuport  F0/L0/F7 mcu! use DigitalPin class"
- #include "digitalPin.h"
- #define BB_PIN DigitalPin
-#endif // F1/2/3/4 L1/4
 #endif //__BITBAND_H__
