a64b1ba7e3d07eaf67478b08e2b141aa4523db91
huaweiwx <huaweiwx@sina.com>
2018-02-25 23:21:23 +0800

    BB_PIN demo for m0/7 use LL_PIN

--- STM32/libraries/BoardExamples/examples/Extension/BB_Option/BB_CLASS/class_demo4/class_demo4.ino
+++ STM32/libraries/BoardExamples/examples/Extension/BB_Option/BB_CLASS/class_demo4/class_demo4.ino
@@ -2,13 +2,13 @@
  * STM32GENERIC arduino extension sample
  * bitband class BB_PIN operator = demo 
  * bitband for m3/4 位域操作仅 m3/4 支持, 直接对位域地址读写，这是最快的位操作；
- *          对 m0/m7，bitband class 调用 DigitalPin class 实现；
+ *          对 m0/m7，bitband class 调用 LL_PIN class 实现；
  * huaweiwx@sina.com 2017.12.26
  */
 
  #include "util/bitband.h"
 
-/*for F0/7 L0 BB_PIN use DigitalPin class replaced */
+/*for F0/7 L0 BB_PIN use LL_PIN class replaced */
 BB_PIN<LED_BUILTIN>led;
 
 void setup() {
