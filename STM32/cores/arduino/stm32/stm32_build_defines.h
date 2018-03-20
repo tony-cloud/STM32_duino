5378488b852fb2534278511ee42e7540d15b9fa2
huaweiwx <huaweiwx@sina.com>
2018-03-20 21:08:10 +0800

    add NUCLEO-L4R5ZI support
    nucleo-L4R5ZI  use LPUART as default serial

--- STM32/cores/arduino/stm32/stm32_build_defines.h
+++ STM32/cores/arduino/stm32/stm32_build_defines.h
@@ -84,6 +84,14 @@
 #define USE_SERIAL6 1
 #endif
 
+#ifndef USE_LPUART1
+ #ifdef STM32L4
+   #define USE_LPUART1 1
+ #else
+   #define USE_LPUART1 0
+ #endif
+#endif
+
 //SPI
 #ifndef USE_SPI1
 #define USE_SPI1 1
@@ -193,7 +201,7 @@
  #define USART_PRIORITY   STM32_INT_PRIORITY
 #endif
 #ifndef SDIO_PRIORITY
- #define SDIO_PRIORITY    STM32_INT_PRIORITY
+ #define SDIO_PRIORITY    STM32_INT_PRIORITY-1
 #endif
 #ifndef DMA2_PRIORITY
  #define DMA2_PRIORITY     STM32_INT_PRIORITY
@@ -232,10 +240,10 @@
 #define RCC_PRIORITY     STM32_INT_PRIORITY
 #endif
 #ifndef EXTI_PRIORITY	/*f1 6~10,23,40*/
-#define EXTI_PRIORITY    MAX_PRIORITY-1  //f1 6~9 botton  use 0x0f
+#define EXTI_PRIORITY    MAX_PRIORITY+1  //f1 6~9 botton  use 0x0f
 #endif
 #ifndef DMA1_PRIORITY   /*f1 11~17*/
-#define DMA1_PRIORITY    STM32_INT_PRIORITY+1
+#define DMA1_PRIORITY    STM32_INT_PRIORITY
 #endif
 #ifndef ADC_PRIORITY    /*f1 18*/
 #define ADC_PRIORITY     STM32_INT_PRIORITY
@@ -262,7 +270,7 @@
 #define SDIO_PRIORITY    STM32_INT_PRIORITY-1
 #endif
 #ifndef DMA2_PRIORITY  /*f1 56~59*/
-#define DMA2_PRIORITY    STM32_INT_PRIORITY+1
+#define DMA2_PRIORITY    STM32_INT_PRIORITY
 #endif
 
 #endif
@@ -4686,8 +4694,15 @@
   #define CMSIS_STARTUP_ASSEMBLY "startup_stm32l4a6xx.s"
   #define CHIP_PERIPHERAL_INCLUDE "stm32_STM32L4A6ZG.h"
   #define CHIP_PIN_LIST PIN(A,0), PIN(A,1), PIN(A,2), PIN(A,3), PIN(A,4), PIN(A,5), PIN(A,6), PIN(A,7), PIN(A,8), PIN(A,9), PIN(A,10), PIN(A,11), PIN(A,12), PIN(A,13), PIN(A,14), PIN(A,15), PIN(B,0), PIN(B,1), PIN(B,2), PIN(B,3), PIN(B,4), PIN(B,5), PIN(B,6), PIN(B,7), PIN(B,8), PIN(B,9), PIN(B,10), PIN(B,11), PIN(B,12), PIN(B,13), PIN(B,14), PIN(B,15), PIN(C,0), PIN(C,1), PIN(C,2), PIN(C,3), PIN(C,4), PIN(C,5), PIN(C,6), PIN(C,7), PIN(C,8), PIN(C,9), PIN(C,10), PIN(C,11), PIN(C,12), PIN(C,13), PIN(C,14), PIN(C,15), PIN(D,0), PIN(D,1), PIN(D,2), PIN(D,3), PIN(D,4), PIN(D,5), PIN(D,6), PIN(D,7), PIN(D,8), PIN(D,9), PIN(D,10), PIN(D,11), PIN(D,12), PIN(D,13), PIN(D,14), PIN(D,15), PIN(E,0), PIN(E,1), PIN(E,2), PIN(E,3), PIN(E,4), PIN(E,5), PIN(E,6), PIN(E,7), PIN(E,8), PIN(E,9), PIN(E,10), PIN(E,11), PIN(E,12), PIN(E,13), PIN(E,14), PIN(E,15), PIN(F,0), PIN(F,1), PIN(F,2), PIN(F,3), PIN(F,4), PIN(F,5), PIN(F,6), PIN(F,7), PIN(F,8), PIN(F,9), PIN(F,10), PIN(F,11), PIN(F,12), PIN(F,13), PIN(F,14), PIN(F,15), PIN(G,0), PIN(G,1), PIN(G,2), PIN(G,3), PIN(G,4), PIN(G,5), PIN(G,6), PIN(G,7), PIN(G,8), PIN(G,9), PIN(G,10), PIN(G,11), PIN(G,12), PIN(G,13), PIN(G,14), PIN(G,15), PIN(H,0), PIN(H,1), PIN(H,3),
+
+#elif defined(STM32L4R5ZI)
+  #define STM32L4R5xx 1
+  #define CMSIS_STARTUP_ASSEMBLY "startup_stm32l4r5xx.s"
+  #define CHIP_PERIPHERAL_INCLUDE "stm32_STM32L4R5ZI.h"
+  #define CHIP_PIN_LIST PIN(A,0), PIN(A,1), PIN(A,2), PIN(A,3), PIN(A,4), PIN(A,5), PIN(A,6), PIN(A,7), PIN(A,8), PIN(A,9), PIN(A,10), PIN(A,11), PIN(A,12), PIN(A,13), PIN(A,14), PIN(A,15), PIN(B,0), PIN(B,1), PIN(B,2), PIN(B,3), PIN(B,4), PIN(B,5), PIN(B,6), PIN(B,7), PIN(B,8), PIN(B,9), PIN(B,10), PIN(B,11), PIN(B,12), PIN(B,13), PIN(B,14), PIN(B,15), PIN(C,0), PIN(C,1), PIN(C,2), PIN(C,3), PIN(C,4), PIN(C,5), PIN(C,6), PIN(C,7), PIN(C,8), PIN(C,9), PIN(C,10), PIN(C,11), PIN(C,12), PIN(C,13), PIN(C,14), PIN(C,15), PIN(D,0), PIN(D,1), PIN(D,2), PIN(D,3), PIN(D,4), PIN(D,5), PIN(D,6), PIN(D,7), PIN(D,8), PIN(D,9), PIN(D,10), PIN(D,11), PIN(D,12), PIN(D,13), PIN(D,14), PIN(D,15), PIN(E,0), PIN(E,1), PIN(E,2), PIN(E,3), PIN(E,4), PIN(E,5), PIN(E,6), PIN(E,7), PIN(E,8), PIN(E,9), PIN(E,10), PIN(E,11), PIN(E,12), PIN(E,13), PIN(E,14), PIN(E,15), PIN(F,0), PIN(F,1), PIN(F,2), PIN(F,3), PIN(F,4), PIN(F,5), PIN(F,6), PIN(F,7), PIN(F,8), PIN(F,9), PIN(F,10), PIN(F,11), PIN(F,12), PIN(F,13), PIN(F,14), PIN(F,15), PIN(G,0), PIN(G,1), PIN(G,2), PIN(G,3), PIN(G,4), PIN(G,5), PIN(G,6), PIN(G,7), PIN(G,8), PIN(G,9), PIN(G,10), PIN(G,11), PIN(G,12), PIN(G,13), PIN(G,14), PIN(G,15), PIN(H,0), PIN(H,1), PIN(H,3),
+
 #else 
-#error UNKNOWN CHIP 
+  #error UNKNOWN CHIP 
 #endif
 
 #endif
